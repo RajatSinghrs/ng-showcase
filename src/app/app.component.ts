69c96a59258255ee0d9979ade9d1a42321382e23
RajatSinghrs <rajatsingh265@gmail.com>
2025-07-29 16:53:41 +0530

    cleanup and images addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
 import { provideQueryClient } from '@ngneat/query';
 import { QueryClient } from '@tanstack/query-core';
 
